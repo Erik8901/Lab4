3ed91b520b31f154e1939572ec8b6ec8e6f1a54a
Erik8901 <32448249+Erik8901@users.noreply.github.com>
2017-12-01 15:22:02 +0100

    updates

--- js/functions.js
+++ js/functions.js
@@ -1,92 +1,54 @@
+            window.addEventListener("load", function() {
 
-   /* window.addEventListener("load", function() {
+                     let btnTEST = document.getElementById("btnTest");
+                     btnTEST.addEventListener("click", function() {
+                       getDataFromDataBase();
+                     });
+                });
 
-
-           function addBook() {
-
-           let req = new XMLHttpRequest();
-
-            let typ = "op=" + "insert";
-            let author = "author=" + "test";
-            let title = "title=" + "title";
-            //let id = "id=" + "11729";
-            let key = "key=" + "hVfr2"
-
-            req.open("GET", "https://www.forverkliga.se/JavaScript/api/crud.php?" + typ+ "&"+ author+ "&"+ title+"&" + key);
-
-           req.send();
-
-           req.onreadystatechange = function(event) {
-               console.log("readyState:" + req.readyState);
-               console.log("status:" + req.status);
-                       console.log("responseText:" + req.responseText);
-
-                       if( req.readyState == 4 ) {
-
-                           console.log(req.response);
-
-                       }
-               };
-           }
-
-         });
-     */
-   window.addEventListener("load", function() {
-
-         let btnTEST = document.getElementById("btnTest");
-         btnTEST.addEventListener("click", function() {
-           getDataFromDataBase();
-         });
-       
-       
-       });
-
-     function deleteBook() {
+                function deleteBook() {
             
-            
-        let btnD = document.getElementById("btnGone");
-        
-        btnD.addEventListener("click",
-             function(event) {
-               var list = document.getElementById("listBooks"); 
-               list.removeChild(list.childNodes[0]);
-        });
-        
-    };
+                    let btnD = document.getElementById("btnGone");
+
+                    btnD.addEventListener("click",
+                         function(event) {
+                           var list = document.getElementById("listBooks"); 
+                           list.removeChild(list.childNodes[0]);
+                    });
+                };
 
 
 
-       var  getDataFromDataBase =  function() {
+                var  getDataFromDataBase =  function() {
          
-         let listBooks = document.getElementById("listBooks");
-         listBooks.innerHTML = ""
+                     let listBooks = document.getElementById("listBooks");
+                     listBooks.innerHTML = ""
 
-         let req = new XMLHttpRequest();
+                     let req = new XMLHttpRequest();
 
-         req.open("GET", "https://www.forverkliga.se/JavaScript/api/crud.php?op=select&" + key);
+                     req.open("GET", "https://www.forverkliga.se/JavaScript/api/crud.php?op=select&" + key);
 
-         req.send();
-         //console.log(req.response);
+                     req.send();
+                     //console.log(req.response);
 
-         req.onreadystatechange = function() {
+                     req.onreadystatechange = function() {
 
-           if (this.readyState == 4 && this.status == 200) {
+                       if (this.readyState == 4 && this.status == 200) {
 
-             //console.log(req.response);
-             let obj = JSON.parse(req.response);
-             console.log(obj);
+                         //console.log(req.response);
+                         let obj = JSON.parse(req.response);
+                         console.log(obj);
 
-             showInUserLib(obj);
-           }
-         }
+                         showInUserLib(obj);
+                       }
+                     }
+                };
 
-       };
+                function showInUserLib(obj) {
+                    console.log(key);
 
-       function showInUserLib(obj) {
-            console.log(key);
-
-         for (let i = 0; i < obj.data.length; i++) {
-             console.log(obj.data[0].id);
+                 for (let i = 0; i < obj.data.length; i++) {
+                     console.log(obj.data[0].id);
 
            let listItem = document.createElement("li");
            let btnRemoveBook = document.createElement("button");
@@ -109,109 +71,50 @@
                     function(event) {
                 
                 
-                console.log(event.target.parentElement);
-                
-                let li = event.target.parentElement;
-                let uniqueBook = li.querySelector(".uniqueBook").innerHTML;                console.log(uniqueBook);
-                
-                
-                removeBook(uniqueBook, li,0);
-                
-                
-                
-                
-            })
-           
-             
-                   
+                        console.log(event.target.parentElement);
 
-           
-         }
-       };
+                        let li = event.target.parentElement;
+                        let uniqueBook = li.querySelector(".uniqueBook").innerHTML;                console.log(uniqueBook);
+
+                        removeBook(uniqueBook, li,0);
+
+                        })
+                    }
+               };
 
 
    
       
-        function removeBook(uniqueBook, li,i) {
+                function removeBook(uniqueBook, li,i) {
                 
-                let link = "https://www.forverkliga.se/JavaScript/api/crud.php?";
-                typ = "&op=delete";
-                id= "&id=" + uniqueBook;
-                
+                    let link = "https://www.forverkliga.se/JavaScript/api/crud.php?";
+                    typ = "&op=delete";
+                    id= "&id=" + uniqueBook;
 
-                fetch(link + key + typ + id).then(function(response){
-                        return response.json();
-          
-                }).then(function(json){
-                console.log(json);
-                    
-                    if(json.status == "error") {
-                        
-                        if(i < 8) {
-                            i++
-                            removeBook(uniqueBook, li,i);
-                        }
-                    } else {
-                    
-                    listBooks.removeChild(li);
-                    
-                }
 
-                }).catch(function(res){
-              
-                });    
-            
-        };
-         
+                    fetch(link + key + typ + id).then(function(response){
+                            return response.json();
+
+                    }).then(function(json){
+                    console.log(json);
+
+                        if(json.status == "error") {
+
+                            if(i < 8) {
+                                i++
+                                removeBook(uniqueBook, li,i);
+                            }
+                        } else {
+
+                        listBooks.removeChild(li);
+
+                    }
+
+                    }).catch(function(res){
+
+                    });    
+                };
+
         
     
-        //window.addEventListener("load", deleteBook) 
-
-        /*    function deleteBook() {
-            
-            
-        let btnD = document.getElementById("btnGone");
-        
-        btnD.addEventListener("click",
-             function(event) {
-            console.log(event);
-               var list = document.getElementById("listBooks"); 
-               list.removeChild(list.childNodes[0]);
-        });
-        
-    };
-*/
-
-  
       
- /*   window.addEventListener("load", event);
-          
-    function event()  { 
-    
-        let id = "";
-        let key = "";
-        let div = document.getElementById("myDiv");    
-    
-        fetch ("https://www.forverkliga.se/JavaScript/api/crud.php?op=delete&" + key + id)
-          .then(
-            function(result) {
-               console.log(response);
-                 return result.json();
-               })
-            .then(function(json) {
-             div.innerHTML = json
-                
-                
-            });
-          };
-          
-          */
-          
-    
-    
-    
- 
-    
-
-
-   
